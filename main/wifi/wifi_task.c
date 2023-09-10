85760294d199597c56f8490a8a3c915f207a5773
Oleksandr Kukharchuk <oldvers@gmail.com>
2023-09-10 22:23:50 +0300

    Added an ability to run WiFi in AP mode.

--- main/wifi/wifi_task.c
+++ main/wifi/wifi_task.c
@@ -18,13 +18,15 @@
 
 //-------------------------------------------------------------------------------------------------
 
-#define WIFI_SSID                "HomeWLAN"
-#define WIFI_PSWD                "wlanH020785endrix!"
-#define WIFI_SITE                "home.com"
-#define EVT_WIFI_STARTED         BIT0
-#define EVT_WIFI_CONNECTED       BIT1
-#define EVT_WIFI_GOT_IP          BIT2
-#define EVT_WIFI_DISCONNECTED    BIT3
+#define WIFI_SSID                    "HomeWLAN"
+#define WIFI_PSWD                    "wlanH020785endrix!"
+#define WIFI_SITE                    "home.com"
+#define EVT_WIFI_ST_STARTED          BIT0
+#define EVT_WIFI_ST_CONNECTED        BIT1
+#define EVT_WIFI_ST_GOT_IP           BIT2
+#define EVT_WIFI_ST_DISCONNECTED     BIT3
+#define EVT_WIFI_AP_ST_CONNECTED     BIT4
+#define EVT_WIFI_AP_ST_DISCONNECTED  BIT5
 
 //-------------------------------------------------------------------------------------------------
 
@@ -86,7 +88,7 @@ static EventBits_t wifi_WaitFor(EventBits_t events, TickType_t timeout)
 
 //-------------------------------------------------------------------------------------------------
 
-static void wifi_OnStarted
+static void wifi_ST_OnStarted
 (
     void* arg,
     esp_event_base_t event_base,
@@ -94,13 +96,13 @@ static void wifi_OnStarted
     void* event_data
 )
 {
-    ESP_LOGI(TAG, "Started!");
-    xEventGroupSetBits(gWiFiEvents, EVT_WIFI_STARTED);
+    ESP_LOGI(TAG, "ST Started!");
+    xEventGroupSetBits(gWiFiEvents, EVT_WIFI_ST_STARTED);
 }
 
 //-------------------------------------------------------------------------------------------------
 
-static void wifi_OnConnected
+static void wifi_ST_OnConnected
 (
     void* arg,
     esp_event_base_t event_base,
@@ -108,13 +110,13 @@ static void wifi_OnConnected
     void* event_data
 )
 {
-    ESP_LOGI(TAG, "Connected to \"%s\"", WIFI_SSID);
-    xEventGroupSetBits(gWiFiEvents, EVT_WIFI_CONNECTED);
+    ESP_LOGI(TAG, "ST Connected to \"%s\"", WIFI_SSID);
+    xEventGroupSetBits(gWiFiEvents, EVT_WIFI_ST_CONNECTED);
 }
 
 //-------------------------------------------------------------------------------------------------
 
-static void wifi_OnGotIp
+static void wifi_ST_OnGotIp
 (
     void* arg,
     esp_event_base_t event_base,
@@ -127,13 +129,13 @@ static void wifi_OnGotIp
     gIpAddr = event->ip_info.ip.addr;
     //ESP_LOGI(TAG, "Got IP: " IPSTR, IP2STR(&event->ip_info.ip));
 
-    ESP_LOGI(TAG, "Got IP : %s", ip4addr_ntoa(&event->ip_info.ip));
-    xEventGroupSetBits(gWiFiEvents, EVT_WIFI_GOT_IP);
+    ESP_LOGI(TAG, "ST Got IP : %s", ip4addr_ntoa(&event->ip_info.ip));
+    xEventGroupSetBits(gWiFiEvents, EVT_WIFI_ST_GOT_IP);
 }    
 
 //-------------------------------------------------------------------------------------------------
 
-static void wifi_OnDisconnected
+static void wifi_ST_OnDisconnected
 (
     void* arg,
     esp_event_base_t event_base,
@@ -141,8 +143,40 @@ static void wifi_OnDisconnected
     void* event_data
 )
 { 
-    ESP_LOGE(TAG, "Disconnected!");
-    xEventGroupSetBits(gWiFiEvents, EVT_WIFI_DISCONNECTED);
+    ESP_LOGE(TAG, "ST Disconnected!");
+    xEventGroupSetBits(gWiFiEvents, EVT_WIFI_ST_DISCONNECTED);
+}
+
+//-------------------------------------------------------------------------------------------------
+
+static void wifi_AP_OnStConnected
+(
+    void* arg,
+    esp_event_base_t event_base,
+    int32_t event_id,
+    void* event_data
+)
+{
+    wifi_event_ap_staconnected_t * event = (wifi_event_ap_staconnected_t *)event_data;
+
+    ESP_LOGE(TAG, "AP - ST Connected! "MACSTR", AID=%d", MAC2STR(event->mac), event->aid);
+    xEventGroupSetBits(gWiFiEvents, EVT_WIFI_AP_ST_CONNECTED);
+}
+
+//-------------------------------------------------------------------------------------------------
+
+static void wifi_AP_OnStDisconnected
+(
+    void* arg,
+    esp_event_base_t event_base,
+    int32_t event_id,
+    void* event_data
+)
+{
+    wifi_event_ap_stadisconnected_t * event = (wifi_event_ap_stadisconnected_t *)event_data;
+
+    ESP_LOGE(TAG, "AP - ST Disconnected! "MACSTR", AID=%d", MAC2STR(event->mac), event->aid);
+    xEventGroupSetBits(gWiFiEvents, EVT_WIFI_AP_ST_DISCONNECTED);
 }
 
 //-------------------------------------------------------------------------------------------------
@@ -153,25 +187,37 @@ static void wifi_RegisterHandlers(void)
     (
         WIFI_EVENT,
         WIFI_EVENT_STA_START,
-        &wifi_OnStarted
+        &wifi_ST_OnStarted
     );
     HNDLR_REG
     (
         WIFI_EVENT,
         WIFI_EVENT_STA_CONNECTED,
-        &wifi_OnConnected
+        &wifi_ST_OnConnected
     );
     HNDLR_REG
     (
         IP_EVENT,
         IP_EVENT_STA_GOT_IP,
-        &wifi_OnGotIp
+        &wifi_ST_OnGotIp
     );
     HNDLR_REG
     (
         WIFI_EVENT,
         WIFI_EVENT_STA_DISCONNECTED,
-        &wifi_OnDisconnected
+        &wifi_ST_OnDisconnected
+    );
+    HNDLR_REG
+    (
+        WIFI_EVENT,
+        WIFI_EVENT_AP_STACONNECTED,
+        &wifi_AP_OnStConnected
+    );
+    HNDLR_REG
+    (
+        WIFI_EVENT,
+        WIFI_EVENT_AP_STADISCONNECTED,
+        &wifi_AP_OnStDisconnected
     );
 }
 
@@ -179,6 +225,11 @@ static void wifi_RegisterHandlers(void)
 
 static void wifi_Start(void)
 {
+    wifi_config_t wifi_config = {0};
+    uint8_t       mac[6]      = {0};
+    char          ap_ssid[32] = {0};
+    int           length      = 0;
+
     /* Prepare the events loop */
     tcpip_adapter_init();
     ESP_ERROR_CHECK(esp_event_loop_create_default());
@@ -189,30 +240,62 @@ static void wifi_Start(void)
     
     /* Register event handler */
     wifi_RegisterHandlers();
-    //HNDLR_REG(WIFI_EVENT, WIFI_EVENT_STA_START, &vWiFi_OnStarted);
-
-    //ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
 
     /* Prepare the WiFi parameters. Temporary in RAM */
     ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
-    wifi_config_t wifi_config =
-    {
-        .sta =
-        {
-            .ssid = WIFI_SSID,
-            .password = WIFI_PSWD
-        },
-    };
 
-    ESP_LOGI(TAG, "Starting...");
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
-    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
-    ESP_ERROR_CHECK(esp_wifi_start());
-    
-    (void)wifi_WaitFor(EVT_WIFI_STARTED, portMAX_DELAY);
-    
-    /* Unregister event handler */
-    //HNDLR_UNREG(WIFI_EVENT, WIFI_EVENT_STA_START, &vWiFi_OnStarted);
+    /* Station mode */
+    if (true == gWiFiConnParams.valid)
+    {
+        //wifi_config_t wifi_config =
+        //{
+        //    .sta =
+        //    {
+        //        .ssid = WIFI_SSID,
+        //        .password = WIFI_PSWD
+        //    },
+        //};
+        memcpy(wifi_config.sta.ssid, gWiFiConnParams.ssid, gWiFiConnParams.ssid_len);
+        memcpy(wifi_config.sta.password, gWiFiConnParams.pswd, gWiFiConnParams.pswd_len);
+
+        ESP_LOGI(TAG, "ST Starting...");
+        ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+        ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+        ESP_ERROR_CHECK(esp_wifi_start());
+
+        (void)wifi_WaitFor(EVT_WIFI_ST_STARTED, portMAX_DELAY);
+
+        ESP_ERROR_CHECK(tcpip_adapter_set_hostname(TCPIP_ADAPTER_IF_STA, "testing"));
+    }
+    else
+    /* Access Point mode */
+    {
+        //wifi_config_t wifi_config =
+        //{
+        //    .ap =
+        //    {
+        //        .ssid = EXAMPLE_ESP_WIFI_SSID,
+        //        .ssid_len = strlen(EXAMPLE_ESP_WIFI_SSID),
+        //        .password = EXAMPLE_ESP_WIFI_PASS,
+        //        .max_connection = EXAMPLE_MAX_STA_CONN,
+        //        .authmode = WIFI_AUTH_WPA_WPA2_PSK
+        //    },
+        //};
+        ESP_ERROR_CHECK(esp_efuse_mac_get_default(mac));
+        length = sprintf(ap_ssid, "WIFI_%02X%02X%02X%02X%02X%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+        memcpy(wifi_config.ap.ssid, ap_ssid, length);
+        wifi_config.ap.ssid_len = length;
+        strcpy((char *)wifi_config.ap.password, "0123456789");
+        wifi_config.ap.max_connection = 1;
+        wifi_config.ap.authmode = WIFI_AUTH_WPA_WPA2_PSK;
+
+        ESP_LOGI(TAG, "AP \"%s\" Starting...", ap_ssid);
+        ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+        ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config));
+        ESP_ERROR_CHECK(esp_wifi_start());
+
+        ESP_ERROR_CHECK(tcpip_adapter_set_hostname(TCPIP_ADAPTER_IF_AP, "testing"));
+    }
 }
 
 //-------------------------------------------------------------------------------------------------
@@ -221,40 +304,45 @@ static FW_BOOLEAN wifi_Connect(void)
 {
     EventBits_t events = 0;
     FW_BOOLEAN result = FW_FALSE;
-    
-    /* Register event handlers */
-    //HNDLR_REG(WIFI_EVENT, WIFI_EVENT_STA_CONNECTED, &vWiFi_OnConnected);
-    //HNDLR_REG(IP_EVENT, IP_EVENT_STA_GOT_IP, &vWiFi_OnGotIp);
-    //HNDLR_REG(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &vWiFi_OnDisconnected);
-    
-    /* Connect */
-    ESP_LOGI(TAG, "Connecting to \"%s\"...", WIFI_SSID);
-    ESP_ERROR_CHECK(esp_wifi_connect());
-    
-    /* Wait for connection */
-    events = wifi_WaitFor(EVT_WIFI_DISCONNECTED | EVT_WIFI_GOT_IP, 10000);
-    if (0 != (events & EVT_WIFI_GOT_IP))
+
+    /* Station mode */
+    if (true == gWiFiConnParams.valid)
     {
-        ESP_LOGI(TAG, "Connected successfuly");
-        result = FW_TRUE;
-    }
-    else if (0 != (events & EVT_WIFI_DISCONNECTED))
-    {
-        ESP_LOGE(TAG, "Reconnection needed");
+        /* Connect */
+        ESP_LOGI(TAG, "Connecting to \"%s\"...", WIFI_SSID);
+        ESP_ERROR_CHECK(esp_wifi_connect());
+    
+        /* Wait for connection */
+        events = wifi_WaitFor(EVT_WIFI_ST_DISCONNECTED | EVT_WIFI_ST_GOT_IP, 10000);
+        if (0 != (events & EVT_WIFI_ST_GOT_IP))
+        {
+            ESP_LOGI(TAG, "Connected successfuly");
+            result = FW_TRUE;
+        }
+        else if (0 != (events & EVT_WIFI_ST_DISCONNECTED))
+        {
+            ESP_LOGE(TAG, "Reconnection needed");
+        }
+        else
+        {
+            ESP_LOGE(TAG, "Something wrong! Reconnection needed");
+        }
     }
     else
+    /* Access Point mode */
     {
-        ESP_LOGE(TAG, "Something wrong! Reconnection needed");
+        /* Wait for connection */
+        events = wifi_WaitFor(EVT_WIFI_AP_ST_CONNECTED, 10000);
+        if (0 != (events & EVT_WIFI_AP_ST_CONNECTED))
+        {
+            ESP_LOGI(TAG, "Connected successfuly");
+            result = FW_TRUE;
+        }
+        else
+        {
+            ESP_LOGE(TAG, "Something wrong! Reconnection needed");
+        }
     }
-    
-    /* Unregister event handlers */
-    //HNDLR_UNREG(WIFI_EVENT, WIFI_EVENT_STA_CONNECTED, &vWiFi_OnConnected);
-    //HNDLR_UNREG(IP_EVENT, IP_EVENT_STA_GOT_IP, &vWiFi_OnGotIp);
-    
-    //if (FW_FALSE == result)
-    //{
-    //    HNDLR_UNREG(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &vWiFi_OnDisconnected);
-    //}
 
     return result;
 }
@@ -263,10 +351,16 @@ static FW_BOOLEAN wifi_Connect(void)
 
 static void wifi_WaitForDisconnect(void)
 {
-    (void)wifi_WaitFor(EVT_WIFI_DISCONNECTED, portMAX_DELAY);
-    
-    /* Unregister event handler */
-    //HNDLR_UNREG(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &vWiFi_OnDisconnected);
+    /* Station mode */
+    if (true == gWiFiConnParams.valid)
+    {
+        (void)wifi_WaitFor(EVT_WIFI_ST_DISCONNECTED, portMAX_DELAY);
+    }
+    else
+    /* Access Point mode */
+    {
+        (void)wifi_WaitFor(EVT_WIFI_AP_ST_DISCONNECTED, portMAX_DELAY);
+    }
 }
 
 //-------------------------------------------------------------------------------------------------
@@ -404,7 +498,7 @@ static void wifi_Task(void * pvParams)
     {
         if (FW_TRUE == wifi_Connect())
         {
-            user_init();
+//            user_init();
 //            UDP_NotifyWiFiIsConnected(gIpAddr);
             wifi_WaitForDisconnect();
         }
@@ -433,7 +527,7 @@ void WIFI_Task_Init(void)
     /* Create the events group for WiFi task */
     gWiFiEvents = xEventGroupCreate();
 
-    //xTaskCreate(wifi_Task, "WiFi", 4096, NULL, 5, NULL);
+    xTaskCreate(wifi_Task, "WiFi", 4096, NULL, 5, NULL);
 }
 
 //-------------------------------------------------------------------------------------------------
